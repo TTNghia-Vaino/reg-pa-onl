a31a816c82f7a5da2c6ad54436e55453b67fdcdd
Vaino <tatrongnghiak15@siu.edu.vn>
2024-11-10 15:07:05 +0700

    Build base code

---
+++ Code/Register-Patient-Online/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Register_Patient_Online.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
