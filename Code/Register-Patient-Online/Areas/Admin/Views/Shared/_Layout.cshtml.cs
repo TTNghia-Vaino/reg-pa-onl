29df64cb1a3980d2d78fafc404ef58f227418aac
Vaino <tatrongnghiak15@siu.edu.vn>
2024-11-11 20:18:11 +0700

    Add area Admin

---
+++ Code/Register-Patient-Online/Areas/Admin/Views/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Register_Patient_Online.Areas.admin.Views.Shared
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
