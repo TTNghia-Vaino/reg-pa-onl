aa6cf110472325ea48f1bd423b282181d0543008
Canh <nnhoang0710@gmail.com>
2024-11-23 23:17:58 +0700

    done

---
+++ Code/Register-Patient-Online/Controllers/GioiThieu.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Register_Patient_Online.Controllers
+{
+    public class GioiThieuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
